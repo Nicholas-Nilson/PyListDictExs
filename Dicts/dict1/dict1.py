04c3ef21be0035ffd2dc8593ee268ce63d5ba272
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-15 17:59:33 -0400

    working through dict1

--- Dicts/dict1/dict1.py
+++ Dicts/dict1/dict1.py
@@ -2,34 +2,44 @@ def create_dict_from_lists(keys, values):
     """
     This function returns a dict made from two lists.
     """
+    return dict(zip(keys,values))
     pass  # implement me
 
 def merge_two_dicts(d1, d2):
     """
     Merge two Python dictionaries into one
     """
-    pass  # implement me
+    d1.update(d2)
+    return d1
 
 def init_dict_with_values(lst, d1):
     """
     Create a dict with default values for each key listed.
 
     """
-    #
-    pass  # implement me
+    # output_dict = {}
+    # for item in lst:
+    #     output_dict[item] = d1
+    # return output_dict
+    output_dict = {item: d1 for item in lst}
+    return output_dict
 
 def extract_keys_to_dict(datadict, keylist):
     """
     Create a dictionary by extracting the keylist from a given dictionary
     """
     #
-    pass  # implement me
+    output_dict = {key: datadict[key] for key in keylist}
+    return output_dict
 
 def delete_keys_from_dict(datadict, keylist):
     """
     Delete a list of keys from a dictionary
     """
-    pass
+    for key in keylist:
+        datadict.pop(key)
+    return datadict
+
 
 def check_dict_for_key(datadict, key):
     """
